8ff340880c3b518f85060095a8f8baa87b2a3511
PhireCryptor <44483309+PhireCryptor@users.noreply.github.com>
2018-10-25 20:16:21 -0300

    Add files via upload

---
+++ comp3613-master/DoctorAppointmentScheduler/src/comp3613/AccessDenied/client/event/PageOneEventHandler.java
@@ -0,0 +1,7 @@
+package comp3613.AccessDenied.client.event;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface PageOneEventHandler extends EventHandler {
+	void onValueSubmit(PageOneEvent event);
+}
